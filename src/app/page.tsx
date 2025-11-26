620b5a916626f686d17259cf45e669c3c4d7d3b3
Dima <skydmytro@gmail.com>
2025-11-26 16:48:50 +0100

    feat: add popular search section with RentCard component and SVG images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/main-page/Hero';
+import PopularSearch from '@/components/main-page/popular-search/PopularSearch';
 
 export default function Home() {
   return (
     <main className="w-full">
       <Hero />
+      <PopularSearch />
     </main>
   );
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+import { PopularSearchCard } from '@/types/types';
+
 export const LOCATION_OPTIONS = [
   { label: 'Casablanca', value: 'casablanca' },
   { label: 'Tangier', value: 'tangier' },
@@ -22,4 +24,22 @@ export const DEAL_TYPE_OPTIONS = [
   { label: 'Buy', value: 'sale' },
   { label: 'Long-time Rent', value: 'long-term' },
   { label: 'Short-time Rent', value: 'short-term' },
+];
+
+export const POPULAR_SEARCH_CARDS: PopularSearchCard[] = [
+  { id: 'buy-apartment', label: 'Buy Apartment', image: '/images/apartment1.svg' },
+  { id: 'buy-house', label: 'Buy House', image: '/images/house1.svg' },
+  { id: 'buy-land', label: 'Buy Land', image: '/images/land.svg' },
+  { id: 'rent-apartment', label: 'Rent Apartment', image: '/images/apartment2.svg' },
+  { id: 'rent-house', label: 'Rent House', image: '/images/house2.svg' },
+  { id: 'rent-office', label: 'Rent Office', image: '/images/office.svg' },
+];
+
+export const GRID_AREA_CLASSES = [
+  'row-[1_/_2] col-[1_/_2] lg:row-[1_/_3] lg:col-[1_/_2]',
+  'row-[2_/_3] col-[1_/_2] lg:row-[1_/_2] lg:col-[2_/_3]',
+  'row-[3_/_4] col-[1_/_2] lg:row-[2_/_3] lg:col-[2_/_3]',
+  'row-[1_/_2] col-[2_/_3] lg:row-[1_/_3] lg:col-[3_/_4]',
+  'row-[2_/_3] col-[2_/_3] lg:row-[1_/_2] lg:col-[4_/_5]',
+  'row-[3_/_4] col-[2_/_3] lg:row-[2_/_3] lg:col-[4_/_5]',
 ];
